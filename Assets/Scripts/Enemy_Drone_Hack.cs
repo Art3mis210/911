db30272bbf27f5fef7f2ffb017f5d45069c48fa4
Art3mis210 <77666553+Art3mis210@users.noreply.github.com>
2021-05-01 15:22:34 +0530

    Added Hacking Feature

---
+++ Assets/Scripts/Enemy_Drone_Hack.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Enemy_Drone_Hack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject Drone;
+    private BoxCollider2D boxC;
+    private void Start()
+    {
+        boxC = GetComponent<BoxCollider2D>();
+    }
+    private void Update()
+    {
+        if(Time.timeScale==0.1f && GameObject.Find("HACK KEYPAD")==false)
+        {
+            DestroyDrone();
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag=="Player" && Input.GetKey(KeyCode.H))
+        {
+            Time.timeScale = 0;
+            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<BoxCollider2D>(), Drone.GetComponent<BoxCollider2D>(), true);
+            SceneManager.LoadScene("HACK", LoadSceneMode.Additive);
+            boxC.enabled = false;
+            
+        }
+    }
+    private void DestroyDrone()
+    {
+        Drone.GetComponent<Animator>().enabled = false;
+        Drone.GetComponent<Rigidbody2D>().gravityScale = 1;
+        enabled = false;
+        Time.timeScale = 1;
+    }
+}
---
+++ Assets/Scripts/Hack.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Hack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private string entered_password;
+    private string orignal_password;
+    public Text Entered_password;
+    public Text Check_password;
+    public Text Orignal_password;
+    void Start()
+    {
+        orignal_password = Random.Range(1000, 9999).ToString();
+        entered_password = "";
+        Orignal_password.text = orignal_password;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Entered_password.text = entered_password;
+    }
+    public void AddDigit(string Digit)
+    {
+            entered_password += Digit;
+    }
+    public void Clear()
+    {
+        entered_password = "";
+    }
+    public void CheckPassword()
+    {
+        if (orignal_password == entered_password)
+        {
+            Check_password.text = "  Success";
+            SceneManager.UnloadSceneAsync("HACK");
+            Time.timeScale = 0.1f;
+        }
+        else
+            Check_password.text = "Incorrect";
+    }
+}
