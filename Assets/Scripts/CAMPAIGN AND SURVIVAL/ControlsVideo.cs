1cb2b7dc12a0f9bd007c87282d978211b84c5783
Art3mis210 <77666553+Art3mis210@users.noreply.github.com>
2021-06-13 14:28:48 +0530

    Completed Control Pages

---
+++ Assets/Scripts/CAMPAIGN AND SURVIVAL/ControlsVideo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class ControlsVideo : MonoBehaviour, ISelectHandler
+{
+    public VideoPlayer VideoP;
+    public VideoClip VideoC;
+    public RawImage RawI;
+    public Text Description;
+    public string DescriptionText;
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        Description.text = DescriptionText;
+        VideoP.Stop();
+        RawI.enabled = false;
+    }
+    public void PlayVideo()
+    {
+        RawI.enabled = true;
+        Description.text = DescriptionText;
+        VideoP.clip = VideoC;
+        VideoP.Play();
+    }
+}
