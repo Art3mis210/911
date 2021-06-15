d43770d15fa62a7cb8075bd2d091be4a1a6dc005
Art3mis210 <77666553+Art3mis210@users.noreply.github.com>
2021-06-16 02:21:30 +0530

    ADDED OBJECTIVES

---
+++ Assets/Scripts/CAMPAIGN AND SURVIVAL/Objective.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Objective : MonoBehaviour
+{
+    private Text ObjectiveText;
+    void Start()
+    {
+        ObjectiveText = GetComponent<Text>();
+        SetObjective();
+    }
+    void SetObjective()
+    {
+        string SceneName = SceneManager.GetActiveScene().name;
+        if (SceneName.Contains("LEVEL 6")) 
+            ObjectiveText.text = "EXECUTE ALEX AND DISARM THE NUKE";
+        else if (SceneName.Contains("LEVEL 4"))
+            ObjectiveText.text = "ESCAPE FROM THE RADIATION";
+        else if (SceneName.Contains("LEVEL 5"))
+            ObjectiveText.text = "ELIMINATE THE POLICE FORCE";
+        else if (SceneName.Contains("LEVEL"))
+            ObjectiveText.text = "ELIMINATE ALL ALEX'S MEN";
+        else if (SceneName.Contains("1-")|| SceneName.Contains("2-"))
+            ObjectiveText.text = "SURVIVE WAVES OF ENEMIES";
+        else if (SceneName.Contains("COOP"))
+            ObjectiveText.text = "ELIMINATE ALL ENEMIES AND DISARM THE BOMB";
+
+    }
+}
